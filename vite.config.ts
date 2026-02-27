332669deb1920cc8f043648842a1433bb820dcd5
Malik <kojomalik@outlook.com>
2026-02-27 13:14:25 +0100

    patch: update vite config disable source map in prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss(), visualizer()],
-});
+  build: {
+    sourcemap: mode !== 'production',
+  },
+}));
